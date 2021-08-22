f9e28b4d40d60fafe79eb6214e38c636493708fd
nehavinod7d <88573323+nehavinod7d@users.noreply.github.com>
2021-08-22 15:46:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var marks = [40,28,75]
+function setup() 
+
+{
+  createCanvas(400,400);
+  var sum = marks[0 ] + marks[1] + marks[2]
+  var avg = sum/marks.length
+  console.log("Avg", avg);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
